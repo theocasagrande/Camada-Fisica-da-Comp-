a843a50070dd0f2ab7a31544c5ea311fea97eb00
theocasagrande <theozito.casagrande@gmail.com>
2026-08-12 12:24:47 -0300

    feito

--- Projeto1/enlaceTx.py
+++ Projeto1/enlaceTx.py
@@ -53,8 +53,20 @@ class TX(object):
         return(len(self.buffer))
 
     def getStatus(self):
+        """ Retorna quantos bytes foram efetivamente transmitidos.
+
+        sendBuffer() apenas sinaliza a thread TX (threadMutex = True) e retorna
+        na hora; quem realmente escreve na porta e atualiza transLen é a thread,
+        de forma assíncrona. Chamar getStatus() logo em seguida, sem esperar,
+        lia transLen antes da thread ter processado o envio (valor 0 ou de um
+        envio anterior). Por isso aguardamos threadMutex voltar a False, que é
+        o sinal de que a thread concluiu a escrita e atualizou transLen.
+        """
+
+        while self.threadMutex:
+            time.sleep(0.001)
         return(self.transLen)
-        
+
 
     def getIsBussy(self):
         return(self.threadMutex)
